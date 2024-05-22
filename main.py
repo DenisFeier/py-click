646980be4c6d9afc1dd5526d78131531b5aec9c0
DenisFeier <67366935+DenisFeier@users.noreply.github.com>
2024-05-22 17:38:15 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,56 +1,94 @@
 import pygame
-
+import time
+from random import randint
 pygame.init()
 
-size = (500, 500)
-
-window = pygame.display.set_mode(size)
-
-rgb = (141, 34, 255)
-
-window.fill(rgb)
-
+back = (200, 255, 255) #background color
+mw = pygame.display.set_mode((500, 500)) #main window
+mw.fill(back)
 clock = pygame.time.Clock()
 
-card_color = (255, 0, 0)
-
-border_color = (0, 255, 0)
-
-text_color = (0, 0, 255)
-
-font = pygame.font.SysFont('verdana', 20)
-
 class Area():
-    def _init_(self, x = 0, y = 0, width = 10, height = 10, color = None):
-        self.rect = pygame.Rect(x, y, width, height)
+    def __init__(self, x=0, y=0, width=10, height=10, color=None):
+        self.rect = pygame.Rect(x, y, width, height) #rectangle
         self.fill_color = color
-        self.shift_x = x + 10
-        self.shift_y = height / 2 + y
-        self.textArea = font.render("Click", True, text_color)
+
 
     def color(self, new_color):
         self.fill_color = new_color
+
+
     def fill(self):
-        pygame.draw.rect(window, self.fill_color, self.rect)
-    def outline(self, frame_color, thickness):
-        pygame.draw.rect(window, frame_color, self.rect, thickness)
-    
-    def drawText(self):
-        window.blit(self.textArea, (self.shift_x, self.shift_y))
+        pygame.draw.rect(mw, self.fill_color, self.rect)
+    def outline(self, frame_color, thickness): #outline of an existing rectangle
+        pygame.draw.rect(mw, frame_color, self.rect, thickness)    
 
-cards = [
-    Area(75, 150, 50, 100, card_color),
-    Area(175, 150, 50, 100, card_color),
-    Area(275, 150, 50, 100, card_color),
-    Area(375, 150, 50, 100, card_color)
-]
 
+    def collidepoint(self, x, y):
+        return self.rect.collidepoint(x, y)       
+
+class Label(Area):
+    def set_text(self, text, fsize=12, text_color=(0, 0, 0)):
+        self.image = pygame.font.SysFont('verdana', fsize).render(text, True, text_color)
+
+
+    def draw(self, shift_x=0, shift_y=0):
+        self.fill()
+        mw.blit(self.image, (self.rect.x + shift_x, self.rect.y + shift_y))
+
+
+RED = (255, 0, 0)
+GREEN = (0, 255, 51)
+YELLOW = (255, 255, 0)
+DARK_BLUE = (0, 0, 100)
+BLUE = (80, 80, 255)
+
+
+cards = []
+num_cards = 4
+
+
+x = 70
+
+
+for i in range(num_cards):
+    new_card = Label(x, 170, 70, 100, YELLOW)
+    new_card.outline(BLUE, 10)
+    new_card.set_text('CLICK', 26)
+    cards.append(new_card)
+    x = x + 100
+
+wait = 0
 while True:
+    if wait == 0:
+        #transferring the label:
+        wait = 20 #how many ticks of the label will be in one place
+        click = randint(1, num_cards)
+        for i in range(num_cards):
+            cards[i].color(YELLOW)
+            if (i + 1) == click:
+                cards[i].draw(10, 40)
+            else:
+                cards[i].fill()
+    else:
+        wait -= 1
+
+
+   #checking the click on each tick:
+    for event in pygame.event.get():
+        if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
+            x, y = event.pos
+            for i in range(num_cards):
+                #looking for the card that the click hit
+                if cards[i].collidepoint(x,y):
+                    if i + 1 == click: # if there is a label on the card, we color it green, add a point
+                        cards[i].color(GREEN)
+                    else: #otherwise color it red, minus a point
+                        cards[i].color(RED)
+
+
+                    cards[i].fill()
+
+
     pygame.display.update()
-
-    for c in cards:
-        c.fill()
-        c.outline(border_color, 5)
-        c.drawText()
-
-    clock.tick(40)
+    clock.tick(40)           
